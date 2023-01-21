187108724e5da0b6c8ab35fd1b324f298808ef17
Ivan Sysoev <ivansysoev007@gmail.com>
2023-01-21 17:07:15 +0300

    progress-bar

--- main image.py
+++ main image.py
@@ -1,28 +1,29 @@
 from PIL import Image
 import openpyxl
 from openpyxl.styles import PatternFill
+from progress.bar import IncrementalBar
+from os.path import abspath
 
-class Painter:
-    def __init__(cls, img):
-        cls.img = img
-    
-    def get_image_data(cls):
-        """Изображение ==> массив RGB значений пикселей"""
-        im = Image.open(cls.img)
-        rgb_code = im.convert("RGB")
-        cls.width, cls.height = im.size
-        return [[rgb_code.getpixel((n,i)) for n in range(cls.width)] for i in range(cls.height)]
-    
-    def color_pixel(cls):
-        wb = openpyxl.Workbook()
-        ws = wb.active
+image = "IMG_0003 (1).jpg"
 
-        for row_num, pixel_row in enumerate(cls.get_image_data()):
-            for column_num, pixel_rgb in enumerate(pixel_row):
-                color = "%02x%02x%02x" % pixel_rgb
-                colorFill = PatternFill(start_color=color, end_color=color, fill_type="solid")
-                ws.cell(row=row_num+1, column=column_num+1).fill = colorFill
+im = Image.open(image)
+rgb_code = im.convert("RGB")
+width, height = im.size
+bar = IncrementalBar("Countdown", max = width * height)
 
-        wb.save(filename="result.xlsx")
-    
-Painter("IMG_0003 (1).jpg").color_pixel()
+wb = openpyxl.Workbook()
+ws = wb.active
+
+for n in range(width):
+    for i in range(height):
+        color = "%02x%02x%02x" % rgb_code.getpixel((i,n))
+        colorFill = PatternFill(start_color=color, end_color=color, fill_type="solid")
+
+        cell = ws.cell(row = n + 1, column = i + 1)
+        cell.fill = colorFill
+        ws.column_dimensions[cell.column_letter].width = 2.9
+        bar.next()
+
+filename = image.rsplit(".", 1)[0] + ".xlsx"
+wb.save(filename=filename)
+print(f"\nOPEN the result: {abspath(filename)}")
